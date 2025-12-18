c8bcaf8aaec90b154422e60ecad43e6d4275697b
Wuif <5678318+wuifdesign@users.noreply.github.com>
2025-12-18 21:08:46 +0100

    wip

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+const fooPost = await queryCollection('content').path('/blog/foo').first()
+</script>
+
 <template>
   About Page
+  <ContentRenderer v-if="fooPost" :value="fooPost" />
+
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import defineCollection from '@nuxt/content';
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
