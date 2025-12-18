28301e7879950928bd78c28841858941a642c427
Wuif <5678318+wuifdesign@users.noreply.github.com>
2025-12-18 21:21:27 +0100

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,4 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/content', '@nuxt/image'],
-  nitro: {
-    preset: 'cloudflare-pages'
-  }
 })
