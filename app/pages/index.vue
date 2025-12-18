cde327371fbc4b115836fb199d3a37b4b70a8162
Wuif <5678318+wuifdesign@users.noreply.github.com>
2025-12-18 20:58:04 +0100

    add fetch

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+const { data } = await useFetch('https://jsonplaceholder.typicode.com/todos/1')
+</script>
+
 <template>
-  Index Page
+  Index Page {{ data }}
 </template>
