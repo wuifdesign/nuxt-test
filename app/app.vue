6164178d2229128e5761c299bbe89e26c0f4dda2
Wuif <5678318+wuifdesign@users.noreply.github.com>
2025-12-18 20:54:38 +0100

    with router

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+</script>
 <template>
   <div>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/">Index</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+      </ul>
+    </nav>
+    <RouterView />
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
     <NuxtTime :datetime="Date.now()" />
