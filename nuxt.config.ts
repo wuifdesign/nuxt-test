99886036a9064eabf4072717cff7afd615ed7e82
Wuif <5678318+wuifdesign@users.noreply.github.com>
2025-12-20 13:34:38 +0100

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,17 @@ import defineCollection from '@nuxt/content';
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/image'],
+  modules: ['@nuxt/content', '@nuxt/image', '@nuxthub/core'],
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
   },
   nitro: {
     preset: 'cloudflare_pages',
   },
+  hub: {
+    cache: {
+      driver: 'cloudflare-kv-binding',
+      namespaceId: 'b63b8618d8934195af9da658c655c5fc'
+    }
+  }
 })
