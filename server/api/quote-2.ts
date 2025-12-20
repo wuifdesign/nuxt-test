c1a0180034b8c59b0c366e4b4844a5edcf527560
Wuif <5678318+wuifdesign@users.noreply.github.com>
2025-12-20 14:40:28 +0100

    wip

---
+++ server/api/quote-2.ts
@@ -0,0 +1,22 @@
+import type { H3Event } from 'h3'
+import type { KVNamespace } from '@cloudflare/workers-types'
+
+export default defineEventHandler(async (event) => {
+  const MY_KV = event.context.cloudflare.env.MY_KV as KVNamespace
+
+  const key = 'v1:data'
+  const cached = await MY_KV.get(key, { type: 'json' })
+
+  if (cached) {
+    return { source: 'kv', data: cached }
+  }
+  const value = await MY_KV.get('hello')
+
+  const quote: any = await $fetch(`https://dummyjson.com/quotes/random`)
+
+  await MY_KV.put(key, JSON.stringify(quote), {
+    expirationTtl: 60
+  })
+
+  return { source: 'fetch', data: quote }
+})
