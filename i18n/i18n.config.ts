edf2c97c07c92dd64954766cb3978175243b7437
Michael Wohlfahrter <m.wohlfahrter@ixopay.com>
2025-12-22 12:18:29 +0100

    wip

--- i18n/i18n.config.ts
+++ i18n/i18n.config.ts
@@ -3,7 +3,7 @@ export default defineI18nConfig(() => {
     fallbackLocale: 'en',
     messageResolver: (obj: any, path) => {
       const msg = path.split('.').reduce((o, k) => o?.[k], obj) as any
-      if(msg?.loc.source === '') {
+      if(msg === '' || msg?.loc?.source === '') {
         return undefined
       }
       return msg
